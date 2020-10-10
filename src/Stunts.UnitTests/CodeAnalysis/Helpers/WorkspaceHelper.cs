b3a32d1267ad8652e186196bb3b59c3c3be64c5d
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-10 18:33:06 -0300

    Cleanup unnecessary workspace-related code

--- src/Stunts.UnitTests/CodeAnalysis/Helpers/WorkspaceHelper.cs
+++ src/Stunts.UnitTests/CodeAnalysis/Helpers/WorkspaceHelper.cs
@@ -44,65 +44,15 @@ public static class WorkspaceHelper
                 (ParseOptions)new CSharpParseOptions(Microsoft.CodeAnalysis.CSharp.LanguageVersion.Latest) :
                 (ParseOptions)new VisualBasicParseOptions(Microsoft.CodeAnalysis.VisualBasic.LanguageVersion.Latest);
 
-        //The location of the .NET assemblies
-        var netstandardPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @".nuget\packages\NETStandard.Library\2.0.3\build\netstandard2.0\ref");
-        if (!Directory.Exists(netstandardPath))
-            netstandardPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @".nuget\packages\NETStandard.Library\2.0.1\build\netstandard2.0\ref");
-        if (!Directory.Exists(netstandardPath))
-            netstandardPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @".nuget\packages\NETStandard.Library\2.0.0\build\netstandard2.0\ref");
-        if (!Directory.Exists(netstandardPath))
-            netstandardPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), @"dotnet\sdk\NuGetFallbackFolder\netstandard.library\2.0.0\build\netstandard2.0\ref");
-        if (!Directory.Exists(netstandardPath))
-            netstandardPath = @"C:\Program Files\dotnet\sdk\NuGetFallbackFolder\netstandard.library\2.0.0\build\netstandard2.0\ref";
-
-        if (!Directory.Exists(netstandardPath))
-            throw new InvalidOperationException("Failed to find location of .NETStandard 2.0 reference assemblies");
-
         var referencePaths =
-            Directory.EnumerateFiles(netstandardPath, "*.dll")
 #pragma warning disable CS0436 // Type conflicts with imported type
-            .Concat(ThisAssembly.Project.BuildReferencePaths.Split('|'))
+            ThisAssembly.Project.BuildReferencePaths.Split('|')
 #pragma warning restore CS0436 // Type conflicts with imported type
             .Where(path => !string.IsNullOrEmpty(path) && File.Exists(path))
             .Distinct(FileNameEqualityComparer.Default);
 
         var projectId = ProjectId.CreateNewId();
         var documents = new List<DocumentInfo>();
-
-        //if (includeStuntApi)
-        //{
-        //    var stuntApi = language == LanguageNames.CSharp ?
-        //        new FileInfo(@"contentFiles\cs\netstandard1.3\Stunts\Stunt.cs").FullName :
-        //        new FileInfo(@"contentFiles\vb\netstandard1.3\Stunts\Stunt.vb").FullName;
-
-        //    Assert.True(File.Exists(stuntApi));
-        //    documents.Add(DocumentInfo.Create(
-        //        DocumentId.CreateNewId(projectId, Path.GetFileName(stuntApi)),
-        //        Path.GetFileName(stuntApi),
-        //        loader: new FileTextLoader(stuntApi, Encoding.Default),
-        //        filePath: stuntApi));
-        //}
-
-        //if (includeMockApi)
-        //{
-        //    var mockApi = language == LanguageNames.CSharp ?
-        //        new FileInfo(@"contentFiles\cs\netstandard2.0\Mocks\Mock.cs").FullName :
-        //        new FileInfo(@"contentFiles\vb\netstandard2.0\Mocks\Mock.vb").FullName;
-        //    var mockApiOverloads = Path.ChangeExtension(mockApi, ".Overloads") + Path.GetExtension(mockApi);
-
-        //    Assert.True(File.Exists(mockApi));
-
-        //    documents.Add(DocumentInfo.Create(
-        //        DocumentId.CreateNewId(projectId, Path.GetFileName(mockApi)),
-        //        Path.GetFileName(mockApi),
-        //        loader: new FileTextLoader(mockApi, Encoding.Default),
-        //        filePath: mockApi));
-        //    documents.Add(DocumentInfo.Create(
-        //        DocumentId.CreateNewId(projectId, Path.GetFileName(mockApiOverloads)),
-        //        Path.GetFileName(mockApiOverloads),
-        //        loader: new FileTextLoader(mockApiOverloads, Encoding.Default),
-        //        filePath: mockApiOverloads));
-        //}
 
         var projectDir = Path.Combine(Path.GetTempPath(), "Test", projectId.Id.ToString());
 
