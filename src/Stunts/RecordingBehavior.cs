38c5f36fbb94c7278e29b0b8af009fa051f3da84
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-11 16:24:42 -0300

    Move RecordingBehavior to main project
    This is a pretty handy simple behavior that will likely be useful in general, so make it part of the main library.

---
+++ src/Stunts/RecordingBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stunts
+{
+    /// <summary>
+    /// Simple behavior that keeps track of all invocations.
+    /// </summary>
+    public class RecordingBehavior : IStuntBehavior
+    {
+        /// <summary>
+        /// A list of all invocations and their result.
+        /// </summary>
+        public List<(IMethodInvocation Invocation, IMethodReturn Return)> Invocations { get; } = new();
+
+        /// <summary>
+        /// Invocation recording applies to all invocations, so it 
+        /// always returns <see langword="true"/>.
+        /// </summary>
+        public bool AppliesTo(IMethodInvocation invocation) => true;
+
+        /// <summary>
+        /// Invokes the next behavior in the pipeline and records 
+        /// the invocation and the result.
+        /// </summary>
+        public IMethodReturn Execute(IMethodInvocation invocation, GetNextBehavior next)
+        {
+            var result = next().Invoke(invocation, next);
+            Invocations.Add((invocation, result));
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the friendly rendering of all invocations performed.
+        /// </summary>
+        public override string ToString() => string.Join(Environment.NewLine, Invocations.Select(i => i.Return.ToString()));
+    }
+}
