a3317bf3e092afe3c475ce9dfde8f8a5930d88ff
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-11 13:50:28 -0300

    Make static factory built-in, reference it by default if supported
    Rather than forcing users to install another package to get the OOB
    compile-time generated proxies, we just automatically reference the
    source generator if supported.

---
+++ src/Stunts/StaticStuntFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Stunts
+{
+    /// <summary>
+    /// Provides a <see cref="IStuntFactory"/> that creates proxies from types 
+    /// generated at compile-time that are included in the received stunt 
+    /// assembly in <see cref="CreateStunt"/>.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class StaticStuntFactory : IStuntFactory
+    {
+        /// <summary>
+        /// Uses the <see cref="StuntNaming.GetFullName(Type, Type[])"/> method to 
+        /// determine the expected full type name of a compile-time generated stunt 
+        /// and tries to locate it from <paramref name="stuntsAssembly"/>.
+        /// </summary>
+        /// <param name="stuntsAssembly">The assembly containing the compile-time generated stunts.</param>
+        /// <param name="baseType">Base type of the stunt.</param>
+        /// <param name="implementedInterfaces">Additional interfaces the stunt implements.</param>
+        /// <param name="construtorArguments">Optional additional constructor arguments for the stunt.</param>
+        public object CreateStunt(Assembly stuntsAssembly, Type baseType, Type[] implementedInterfaces, object?[] construtorArguments)
+        {
+            var name = StuntNaming.GetFullName(baseType, implementedInterfaces);
+            var type = stuntsAssembly.GetType(name, true, false);
+
+            return Activator.CreateInstance(type, construtorArguments);
+        }
+    }
+}
