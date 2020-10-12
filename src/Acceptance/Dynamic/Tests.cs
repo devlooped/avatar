53fb3c166b5aa46c23b8286bf8b5a32d7d8cb763
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-12 01:17:37 -0300

    Add also a dynamic stunt acceptance test

---
+++ src/Acceptance/Dynamic/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Stunts;
+using Xunit;
+
+namespace Sample
+{
+    public class Tests
+    {
+        [Fact]
+        public void CanConfigureDefaultValues()
+        {
+            var calculator = Stunt.Of<ICalculator, IDisposable>();
+
+            Assert.IsNotType<StaticStuntFactory>(StuntFactory.Default);
+
+            var recorder = new RecordingBehavior();
+
+            calculator.AddBehavior(recorder);
+            calculator.AddBehavior(new DefaultValueBehavior());
+
+            Assert.IsAssignableFrom<IDisposable>(calculator);
+
+            Assert.Equal(0, calculator.Add(5, 10));
+            Assert.Single(recorder.Invocations);
+
+            Console.WriteLine(recorder.ToString());
+        }
+    }
+}
