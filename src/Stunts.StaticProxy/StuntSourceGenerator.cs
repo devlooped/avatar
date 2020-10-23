59f12ef6d73f3ee6a98aa2909d63ad5645521f6b
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-23 17:00:21 -0300

    Improve extensibility of the source generator

--- src/Stunts.StaticProxy/StuntSourceGenerator.cs
+++ src/Stunts.StaticProxy/StuntSourceGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -19,51 +20,110 @@ namespace Stunts
     [Generator]
     public class StuntSourceGenerator : ISourceGenerator
     {
-        static string? resolveDir;
+        static HashSet<string> resolveDirs = new();
 
         static StuntSourceGenerator() => AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
 
         static Assembly? OnAssemblyResolve(object? sender, ResolveEventArgs args)
         {
-            if (resolveDir == null)
+            if (resolveDirs.Count == 0)
                 return null;
 
             var name = new AssemblyName(args.Name).Name;
             if (name == null)
                 return null;
 
-            var file = Path.GetFullPath(Path.Combine(resolveDir, name + ".dll"));
-
-            if (File.Exists(file))
-                return Assembly.LoadFrom(file);
+            foreach (var dir in resolveDirs)
+            {
+                var file = Path.GetFullPath(Path.Combine(dir, name + ".dll"));
+                if (File.Exists(file))
+                    return Assembly.LoadFrom(file);
+            }
 
             return null;
         }
 
+        /// <summary>
+        /// Adds a directory to the <see cref="AppDomain.AssemblyResolve"/> probing paths when 
+        /// loading the generator.
+        /// </summary>
+        /// <returns>Whether the directory was added or it was already registered.</returns>
+        public static bool AddResolveDirectory(string resolveDirectory) => resolveDirs.Add(resolveDirectory);
+
+        protected virtual StuntDocumentGenerator DocumentGenerator => new StuntDocumentGenerator();
+
+        public virtual void Initialize(GeneratorInitializationContext context)
+            => context.RegisterForSyntaxNotifications(() => new StuntGeneratorReceiver());
+
         [MethodImpl(MethodImplOptions.NoInlining)]
-        public void Execute(GeneratorExecutionContext context)
+        public virtual void Execute(GeneratorExecutionContext context)
         {
             context.AnalyzerConfigOptions.CheckDebugger(nameof(StuntSourceGenerator));
 
             if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.StuntAnalyzerDir", out var analyerDir))
-                resolveDir = analyerDir;
-
-            OnExecute(context);
-        }
-
-        public void Initialize(GeneratorInitializationContext context) 
-            => context.RegisterForSyntaxNotifications(() => new StuntGeneratorReceiver());
-
-        void OnExecute(GeneratorExecutionContext context)
-        {
+                AddResolveDirectory(analyerDir);
 
             if (context.SyntaxReceiver is not StuntGeneratorReceiver receiver)
                 return;
 
-            var generatorAttr = context.Compilation.GetTypeByMetadataName("Stunts.StuntGeneratorAttribute");
+             OnExecute(context, DocumentGenerator);
+        }
+
+        void OnExecute(GeneratorExecutionContext context, StuntDocumentGenerator generator)
+        {
+            if (context.SyntaxReceiver is not StuntGeneratorReceiver receiver)
+                return;
+
+            if (generator.GeneratorAttribute.FullName == null)
+                return;
+
+            var generatorAttr = context.Compilation.GetTypeByMetadataName(generator.GeneratorAttribute.FullName);
             if (generatorAttr == null)
                 return;
 
+            bool CanGenerateFor(INamedTypeSymbol? symbol)
+            {
+                if (symbol == null)
+                    return false;
+
+                // Cannot generate for types using pointer types
+                var usesPointers = symbol.GetMembers()
+                    .OfType<IMethodSymbol>()
+                    .SelectMany(method => method.Parameters)
+                    .Any(parameter => parameter.Type.Kind == SymbolKind.PointerType);
+
+                return !usesPointers;
+            }
+
+            IEnumerable<INamedTypeSymbol[]> GetCandidates()
+            {
+                foreach (var invocation in receiver.Invocations)
+                {
+                    var semantic = context.Compilation.GetSemanticModel(invocation.Node.SyntaxTree);
+                    var symbol = semantic.GetSymbolInfo(invocation.Node, context.CancellationToken);
+                    if (symbol.Symbol is not IMethodSymbol method)
+                        continue;
+
+                    if (!method.GetAttributes().Any(attr => SymbolEqualityComparer.Default.Equals(attr.AttributeClass, generatorAttr)))
+                        continue;
+
+                    var typeArgs = invocation.Name.TypeArgumentList.Arguments
+                        .Select(name => semantic.GetSymbolInfo(name, context.CancellationToken).Symbol as INamedTypeSymbol)
+                        .ToList();
+
+                    // A corresponding diagnostics analyzer would flag this as a compile error.
+                    if (!typeArgs.All(CanGenerateFor))
+                        continue;
+
+                    yield return typeArgs.Cast<INamedTypeSymbol>().ToArray();
+                }
+            };
+
+            OnExecute(context, generator, GetCandidates());
+        }
+
+        protected virtual void OnExecute(GeneratorExecutionContext context, StuntDocumentGenerator generator, IEnumerable<INamedTypeSymbol[]> candidates)
+        {
             using var workspace = new AdhocWorkspace(WorkspaceServices.HostServices);
 
             var projectId = ProjectId.CreateNewId();
@@ -74,7 +134,7 @@ namespace Stunts
                 filePath: tree.FilePath,
                 loader: TextLoader.From(TextAndVersion.Create(tree.GetText(), VersionStamp.Create()))))
                 .ToArray();
-            
+
             var projectInfo = ProjectInfo.Create(
                 projectId, VersionStamp.Create(),
                 nameof(StuntSourceGenerator),
@@ -92,81 +152,39 @@ namespace Stunts
                 // TODO: report, should never happen
                 return;
 
-            var generator = new StuntDocumentGenerator();
             var generatedStunts = new HashSet<string>();
 
-            foreach (var invocation in receiver.Invocations)
+            foreach (var args in candidates)
             {
-                var semantic = context.Compilation.GetSemanticModel(invocation.Node.SyntaxTree);
-                var symbol = semantic.GetSymbolInfo(invocation.Node, context.CancellationToken);
-                if (symbol.Symbol is not IMethodSymbol method)
+                var name = generator.NamingConvention.GetName(args);
+                if (generatedStunts.Contains(name))
                     continue;
 
-                if (!method.GetAttributes().Any(attr => SymbolEqualityComparer.Default.Equals(attr.AttributeClass, generatorAttr)))
-                    continue;
-
-                var typeArgs = invocation.Name.TypeArgumentList.Arguments
-                    .Select(name => semantic.GetSymbolInfo(name, context.CancellationToken).Symbol as INamedTypeSymbol)
-                    .ToList();
-
-                // A corresponding diagnostics analyzer would flag this as a compile error.
-                if (!typeArgs.All(CanGenerateFor))
-                    continue;
-
-                // Check if the type hasn't been generated already
-                if (generatedStunts.Contains(generator.NamingConvention.GetName(typeArgs.OfType<ITypeSymbol>())))
-                    continue;
-
-                // Convert from real compilation symbols to ad-hoc one for codegen.
-                var finalArgs = typeArgs.Select(x => x!.ToFullName())
-                    .Select(compilation.GetTypeByFullName)
-                    .OfType<INamedTypeSymbol>()
-                    .ToList();
-
-                // Shouldn't happen.
-                if (finalArgs.Any(x => x == null))
-                    continue;
-
-                var doc = generator.GenerateDocumentAsync(project, finalArgs.ToArray(), context.CancellationToken).Result;
+                var doc = generator.GenerateDocumentAsync(project, args.ToArray(), context.CancellationToken).Result;
                 var root = doc.GetSyntaxRootAsync(context.CancellationToken).Result;
                 if (root == null)
                     continue;
 
                 var code = root.NormalizeWhitespace().ToFullString();
-                var name = generator.NamingConvention.GetName(finalArgs);
                 if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("build_property.EmitStuntSource", out var emitSource) &&
                     bool.TryParse(emitSource, out var shouldEmit) &&
                     shouldEmit)
                 {
                     var filePath = Path.Combine(Path.GetTempPath(), name + ".cs");
                     File.WriteAllText(filePath, code);
-                    context.ReportDiagnostic(Diagnostic.Create("ST424242", "Compiler", $"{filePath}", 
-                        DiagnosticSeverity.Warning, DiagnosticSeverity.Warning, true, 4, 
+                    context.ReportDiagnostic(Diagnostic.Create("ST424242", "Compiler", filePath,
+                        DiagnosticSeverity.Warning, DiagnosticSeverity.Warning, true, 4,
                         location: Location.Create(filePath, TextSpan.FromBounds(0, 0), new LinePositionSpan())));
                 }
 
                 generatedStunts.Add(name);
                 context.AddSource(name, code);
             }
-
-            bool CanGenerateFor(INamedTypeSymbol? symbol)
-            {
-                if (symbol == null)
-                    return false;
-
-                // Cannot generate for types using pointer types
-                var usesPointers = symbol.GetMembers()
-                    .OfType<IMethodSymbol>()
-                    .SelectMany(method => method.Parameters)
-                    .Any(parameter => parameter.Type.Kind == SymbolKind.PointerType);
-
-                return !usesPointers;
-            }
         }
 
         class StuntGeneratorReceiver : ISyntaxReceiver
         {
-            public List<(SyntaxNode Node, GenericNameSyntax Name)> Invocations { get; } = new();
+            public List<(InvocationExpressionSyntax Node, GenericNameSyntax Name)> Invocations { get; } = new();
 
             public void OnVisitSyntaxNode(SyntaxNode node)
             {
