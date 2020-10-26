01a4e377923d7d504d184b99c282d578a0b66566
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-26 01:58:17 -0300

    Disable nullable annotations on manual stunts
    These are just a sample hand-coded stunt, no need for it to
    adhere to nullable annotations/constraints. It's only used
    in tests.

--- src/ManualStunts/CalculatorBase.cs
+++ src/ManualStunts/CalculatorBase.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿#nullable disable
+using System;
 
 namespace Sample
 {
     public abstract class CalculatorBase
     {
 #pragma warning disable CS0067
-        public virtual event EventHandler? TurnedOn;
+        public virtual event EventHandler TurnedOn;
 #pragma warning restore CS0067
 
         public abstract bool IsOn { get; }
