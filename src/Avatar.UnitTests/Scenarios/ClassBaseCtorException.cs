096efa16020a799368b9b0dced6f6ffed746dc4c
Daniel Cazzulino <daniel@cazzulino.com>
2020-11-04 02:13:34 -0300

    Improve static factory exception thrown when instantiating avatar
    This avoids the calling code having to always catch TargetInvocationException instead of the specific expected exceptions.

---
+++ src/Avatar.UnitTests/Scenarios/ClassBaseCtorException.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS0436
+using System;
+using Xunit;
+
+namespace Avatars.Scenarios.ClassBaseCtorException
+{
+    public class ClassBaseCtor
+    {
+        public ClassBaseCtor() => throw new IndexOutOfRangeException();
+    }
+
+    public class Test : IRunnable
+    {
+        public void Run() 
+            => Assert.Throws<IndexOutOfRangeException>(() => Avatar.Of<ClassBaseCtor>());
+    }
+}
