e8781cc6e57427fc0f2ecf0a3b826d2badec2b61
Daniel Cazzulino <daniel@cazzulino.com>
2020-11-16 15:38:44 -0300

    Bump dependencies to net5 RTW

---
+++ src/Avatar.UnitTests/SpansTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Xunit;
+
+namespace Avatars
+{
+    public class SpansTests
+    {
+        public class PointWrapper
+        {
+            public double X;
+            public double Y;
+        }
+
+        public ref struct Point
+        {
+            public double X;
+            public double Y;
+
+            public Point(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void Flip()
+            {
+                var z = X;
+                Y = X;
+                X = z;
+            }
+        }
+
+        [Fact]
+        public void SpanApi()
+        {
+            var span = new ReadOnlySpan<string>(new[] { "foo", "bar" });
+            var owner = MemoryPool<string>.Shared.Rent();
+            var arguments = new Arguments(typeof(SpanAPI).GetMethods().First().GetParameters())
+            {
+                { "foo", span },
+                { "owner", owner },
+            };
+
+            var point = new Point { X = 5, Y = 10 };
+            var point2 = default(Point);
+
+            Calculate(ref point);
+            Calculate(ref point2);
+        }
+
+        void Calculate(ref Point point)
+        {
+        }
+
+        public class SpanAPI
+        {
+            public string GetSpan(ReadOnlySpan<string> span) => span[5];
+        }
+
+        delegate ReadOnlySpan<T> GetReadOnlySpan<T>();
+
+        public class Arguments : IEnumerable
+        {
+            readonly ParameterInfo[] infos;
+            readonly Dictionary<string, Delegate> getters = new Dictionary<string, Delegate>();
+
+            public Arguments(ParameterInfo[] infos)
+            {
+                this.infos = infos;
+            }
+
+            public void Add<T>(string name, ReadOnlySpan<T> span)
+            {
+                // TODO: populate mirror state, save
+            }
+
+            public void Add<T>(string name, Span<T> span)
+            {
+                // TODO: populate mirror state, save
+            }
+
+            public void Add<T>(string name, Memory<T> memory)
+            {
+                // TODO: populate mirror state, save
+            }
+
+            public void Add<T>(string name, ReadOnlyMemory<T> memory)
+            {
+                // TODO: populate mirror state, save
+            }
+
+            public void Add<T>(string name, IMemoryOwner<T> owner)
+            {
+                // TODO: populate mirror state, save
+            }
+
+            public ReadOnlySpan<T> GetReadOnlySpan<T>(string name)
+            {
+                // TODO: retrieve state from mirror
+                return default;
+            }
+
+            public Span<T> GetSpan<T>(string name)
+            {
+                // TODO: retrieve state from mirror
+                return default;
+            }
+
+            public Memory<T> GetMemory<T>(string name)
+            {
+                // TODO: retrieve state from mirror
+                return default;
+            }
+
+            public ReadOnlyMemory<T> GetReadOnlyMemory<T>(string name)
+            {
+                // TODO: retrieve state from mirror
+                return default;
+            }
+
+            public IMemoryOwner<T> GetMemoryOwner<T>(string name)
+            {
+                // TODO: retrieve state from mirror
+                return default;
+            }
+
+
+
+
+            public IEnumerator GetEnumerator()
+            {
+                return infos.GetEnumerator();
+            }
+        }
+    }
+}
--- src/ManualAvatars/CalculatorClassAvatar.cs
+++ src/ManualAvatars/CalculatorClassAvatar.cs
@@ -14,39 +14,49 @@ namespace Sample
 
         public override event EventHandler TurnedOn
         {
-            add => pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), value), (m, n) => { base.TurnedOn += value; return m.CreateValueReturn(null, value); });
-            remove => pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), value), (m, n) => { base.TurnedOn -= value; return m.CreateValueReturn(null, value); });
+            add => pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), value), (m, n) => { base.TurnedOn += value; return m.CreateValueReturn(null, m.Arguments); });
+            remove => pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), value), (m, n) => { base.TurnedOn -= value; return m.CreateValueReturn(null, m.Arguments); });
         }
 
         public override CalculatorMode Mode
         {
             get => pipeline.Execute<CalculatorMode>(new MethodInvocation(this, MethodBase.GetCurrentMethod()), (m, n) => m.CreateValueReturn(base.Mode));
-            set => pipeline.Invoke(new MethodInvocation(this, MethodBase.GetCurrentMethod(), value), (m, n) => { base.Mode = value; return m.CreateValueReturn(null, value); });
+            set => pipeline.Invoke(new MethodInvocation(this, MethodBase.GetCurrentMethod(), value), (m, n) => { base.Mode = value; return m.CreateValueReturn(null, m.Arguments); });
         }
 
         public override int? this[string name]
         {
             get => pipeline.Execute<int?>(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name), (m, n) => m.CreateValueReturn(base[name]));
-            set => pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name, value), (m, n) => { base[name] = value; return m.CreateValueReturn(null, value); });
+            set => pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name, value), (m, n) => { base[name] = value; return m.CreateValueReturn(null, m.Arguments); });
         }
 
         public override bool IsOn => pipeline.Execute<bool>(new MethodInvocation(this, MethodBase.GetCurrentMethod()), (m, n) => m.CreateValueReturn(base.IsOn));
 
         public override int Add(int x, int y) =>
-            pipeline.Execute<int>(new MethodInvocation(this, MethodBase.GetCurrentMethod(), x, y), (m, n) => m.CreateValueReturn(base.Add(x, y), x, y));
+            pipeline.Execute<int>(new MethodInvocation(this, MethodBase.GetCurrentMethod(), x, y), (m, n) => m.CreateValueReturn(base.Add(x, y), m.Arguments));
 
         public override int Add(int x, int y, int z) =>
-            pipeline.Execute<int>(new MethodInvocation(this, MethodBase.GetCurrentMethod(), x, y, z), (m, n) => m.CreateValueReturn(base.Add(x, y, z), x, y, z));
+            pipeline.Execute<int>(new MethodInvocation(this, MethodBase.GetCurrentMethod(), x, y, z), (m, n) => m.CreateValueReturn(base.Add(x, y, z), m.Arguments));
 
         public override bool TryAdd(ref int x, ref int y, out int z)
         {
+            var method = MethodBase.GetCurrentMethod();
             z = default;
-            var local_x = x;
-            var local_y = y;
-            var local_z = z;
 
-            var result = pipeline.Invoke(new MethodInvocation(this, MethodBase.GetCurrentMethod(), x, y, z),
-                (m, n) => m.CreateValueReturn(base.TryAdd(ref local_x, ref local_y, out local_z), local_x, local_y, local_z), true);
+            var result = pipeline.Invoke(new MethodInvocation(this, method, x, y, z),
+                (m, n) => 
+                {
+                    var local_x = m.Arguments.Get<int>("x");
+                    var local_y = m.Arguments.Get<int>("y");
+                    var local_z = m.Arguments.Get<int>("z");
+                    return m.CreateValueReturn(base.TryAdd(ref local_x, ref local_y, out local_z), 
+                        new ArgumentCollection(method.GetParameters())
+                        {
+                            { "x", local_x },
+                            { "y", local_y },
+                            { "z", local_z },
+                        }); 
+                }, true);
 
             x = result.Outputs.GetNullable<int>("x");
             y = result.Outputs.GetNullable<int>("y");
@@ -59,13 +69,13 @@ namespace Sample
             pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod()), (m, n) => { base.TurnOn(); return m.CreateValueReturn(null); });
 
         public override void Store(string name, int value) =>
-            pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name, value), (m, n) => { base.Store(name, value); return m.CreateValueReturn(null, name, value); });
+            pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name, value), (m, n) => { base.Store(name, value); return m.CreateValueReturn(null, m.Arguments); });
 
         public override int? Recall(string name) =>
-            pipeline.Execute<int?>(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name), (m, n) => m.CreateValueReturn(base.Recall(name), name));
+            pipeline.Execute<int?>(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name), (m, n) => m.CreateValueReturn(base.Recall(name), m.Arguments));
 
         public override void Clear(string name) =>
-            pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name), (m, n) => { base.Clear(name); return m.CreateValueReturn(null, name); });
+            pipeline.Execute(new MethodInvocation(this, MethodBase.GetCurrentMethod(), name), (m, n) => { base.Clear(name); return m.CreateValueReturn(null, m.Arguments); });
 
         public override ICalculatorMemory Memory
         {
