0dc2e08ba636ce445f77aa8554cf63d43fa0d0ae
Daniel Cazzulino <daniel@cazzulino.com>
2021-01-06 19:15:02 -0300

    🧹 Fix formatting by updating dotnet format to 5.0
    Applied oss template

--- src/Avatar/MethodReturn.cs
+++ src/Avatar/MethodReturn.cs
@@ -15,90 +15,90 @@ namespace Avatars
     {
         readonly IMethodInvocation invocation;
 
-    /// <summary>
-    /// Initializes a value returning call.
-    /// </summary>
-    /// <param name="invocation">The invocation that generated the return.</param>
-    /// <param name="returnValue">Optional return value for non-void methods.</param>
-    /// <param name="arguments">All arguments of the invocation, including ref/out ones.</param>
-    public MethodReturn(IMethodInvocation invocation, object? returnValue, IArgumentCollection arguments)
-    {
-        if (invocation.MethodBase.GetParameters().Length != arguments.Count)
-            throw new ArgumentException(ThisAssembly.Strings.MethodArgumentsMismatch(invocation.MethodBase.Name, invocation.MethodBase.GetParameters().Length, arguments.Count), nameof(arguments));
+        /// <summary>
+        /// Initializes a value returning call.
+        /// </summary>
+        /// <param name="invocation">The invocation that generated the return.</param>
+        /// <param name="returnValue">Optional return value for non-void methods.</param>
+        /// <param name="arguments">All arguments of the invocation, including ref/out ones.</param>
+        public MethodReturn(IMethodInvocation invocation, object? returnValue, IArgumentCollection arguments)
+        {
+            if (invocation.MethodBase.GetParameters().Length != arguments.Count)
+                throw new ArgumentException(ThisAssembly.Strings.MethodArgumentsMismatch(invocation.MethodBase.Name, invocation.MethodBase.GetParameters().Length, arguments.Count), nameof(arguments));
 
-        this.invocation = invocation;
+            this.invocation = invocation;
 
-        ReturnValue = returnValue;
-        Outputs = GetOutputs(arguments);
-    }
+            ReturnValue = returnValue;
+            Outputs = GetOutputs(arguments);
+        }
 
-    /// <summary>
-    /// Initializes a return that represents a thrown exception from the invocation.
-    /// </summary>
-    /// <param name="invocation">The invocation that generated the return.</param>
-    /// <param name="exception">The exception resulting from the invocation.</param>
-    public MethodReturn(IMethodInvocation invocation, Exception exception)
-    {
-        this.invocation = invocation;
-        Outputs = GetOutputs(invocation.Arguments);
-        Exception = exception;
-    }
+        /// <summary>
+        /// Initializes a return that represents a thrown exception from the invocation.
+        /// </summary>
+        /// <param name="invocation">The invocation that generated the return.</param>
+        /// <param name="exception">The exception resulting from the invocation.</param>
+        public MethodReturn(IMethodInvocation invocation, Exception exception)
+        {
+            this.invocation = invocation;
+            Outputs = GetOutputs(invocation.Arguments);
+            Exception = exception;
+        }
 
-    /// <inheritdoc />
-    public IArgumentCollection Outputs { get; init; }
+        /// <inheritdoc />
+        public IArgumentCollection Outputs { get; init; }
 
-    /// <inheritdoc />
-    public object? ReturnValue { get; init; }
+        /// <inheritdoc />
+        public object? ReturnValue { get; init; }
 
-    /// <inheritdoc />
-    public Exception? Exception { get; init; }
+        /// <inheritdoc />
+        public Exception? Exception { get; init; }
 
-    /// <inheritdoc />
-    public IDictionary<string, object> Context => invocation.Context;
+        /// <inheritdoc />
+        public IDictionary<string, object> Context => invocation.Context;
 
-    /// <summary>
-    /// Gets a friendly representation of the object.
-    /// </summary>
-    /// <devdoc>
-    /// We don't want to optimize code coverage for this since it's a debugger aid only. 
-    /// Annotating this method with DebuggerNonUserCode achieves that.
-    /// No actual behavior depends on these strings.
-    /// </devdoc>
-    [ExcludeFromCodeCoverage]
+        /// <summary>
+        /// Gets a friendly representation of the object.
+        /// </summary>
+        /// <devdoc>
+        /// We don't want to optimize code coverage for this since it's a debugger aid only. 
+        /// Annotating this method with DebuggerNonUserCode achieves that.
+        /// No actual behavior depends on these strings.
+        /// </devdoc>
+        [ExcludeFromCodeCoverage]
 #if !DEBUG
     [DebuggerNonUserCode]
 #endif
-    public override string ToString()
-    {
-        var result = new StringBuilder();
-        result.Append(invocation.ToString());
-
-        if (Exception != null)
+        public override string ToString()
         {
-            result.Append($" => throw new {Exception.GetType().Name}(\"{Exception.Message}\")");
+            var result = new StringBuilder();
+            result.Append(invocation.ToString());
+
+            if (Exception != null)
+            {
+                result.Append($" => throw new {Exception.GetType().Name}(\"{Exception.Message}\")");
+            }
+            else if (invocation.MethodBase is MethodInfo r && r.ReturnType != typeof(void))
+            {
+                result.Append(" => ")
+                    .Append(
+                        ReturnValue == null ? "null" :
+                        r.ReturnType == typeof(string) ? $"\"{ReturnValue}\"" :
+                        r.ReturnType == typeof(bool) ? ReturnValue.ToString().ToLowerInvariant() :
+                        ReturnValue);
+            }
+
+            return result.ToString();
         }
-        else if (invocation.MethodBase is MethodInfo r && r.ReturnType != typeof(void))
+
+        IArgumentCollection GetOutputs(IArgumentCollection arguments)
         {
-            result.Append(" => ")
-                .Append(
-                    ReturnValue == null ? "null" :
-                    r.ReturnType == typeof(string) ? $"\"{ReturnValue}\"" :
-                    r.ReturnType == typeof(bool) ? ReturnValue.ToString().ToLowerInvariant() :
-                    ReturnValue);
+            var outputs = new ArgumentCollection(invocation.MethodBase.GetParameters()
+                .Where(x => x.ParameterType.IsByRef || x.IsOut).ToArray());
+
+            foreach (var info in outputs)
+                outputs.Add(info.Name, arguments.GetValue(info.Name));
+
+            return outputs;
         }
-
-        return result.ToString();
     }
-
-    IArgumentCollection GetOutputs(IArgumentCollection arguments)
-    {
-        var outputs = new ArgumentCollection(invocation.MethodBase.GetParameters()
-            .Where(x => x.ParameterType.IsByRef || x.IsOut).ToArray());
-
-        foreach (var info in outputs)
-            outputs.Add(info.Name, arguments.GetValue(info.Name));
-
-        return outputs;
-    }
-}
 }
